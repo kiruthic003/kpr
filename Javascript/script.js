fae1d3aa58a1e01a7a69c465247b9f2200db5f9d
kiruthic <bndhaeaneesh@gmail.com>
2025-06-16 15:11:21 +0530

    first commit

---
+++ Javascript/script.js
@@ -0,0 +1,46 @@
+
+
+// var marks =[90,95,96,100,99];
+// var [m1,m2,m3,m4,m5] =marks;
+// console.log(m1)
+// console.log(m2)
+// console.log(m3)
+// console.log(m4)
+// console.log(m5)
+
+
+// // let arr = [10,20,30,40];
+// // let arr2 = [...arr,50,60,70]
+// // console.log(arr)
+// // console.log(arr2)
+
+
+// // // function sum(a,b){
+// // //     console.log(a+b)
+// // // }
+// // // sum(10,20)
+
+// // // var sum = ()=>{
+// // //     var a=10;
+// // //     var b=20;
+// // //     console.log(a+b)
+// // // }
+// // // sum();
+
+// // // // var a="10";
+// // // // var b=10
+// // // // console.log(a+b);
+
+// // // // console.log(a==b);
+// // // // console.log(a===b);
+// // // // console.log(a!=b);
+// // // // console.log(a!==b);
+
+// // // // var num=2;
+// // // // var result = num%2===0? "even":"odd"
+// // // // console.log(result)
+
+// // // // var mark=34;
+// // // // var result = (mark>=80)?"O":(mark>=60)?"a+":(mark>=35)?"pass":"FAIL"
+// // // //     console.log(result)
+
